dd5ec6305b7dba33aafd7a96608cad5867db0ef0
Christian Eckenrode Work <christianeckenrodework@Christians-MacBook-Pro.local>
2016-05-08 19:14:27 -0400

    connected books reducer to book-list container and redux

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Component } from 'react';
+import {Component} from 'react';
+
+import BookList from '../containers/book-list';
 
 export default class App extends Component {
   render() {
-    return (
-      <div>React simple starter</div>
-    );
+    return (<BookList />);
   }
 }
